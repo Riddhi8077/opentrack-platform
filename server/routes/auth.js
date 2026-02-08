c28213842cfbae362e3a7a7da4a41da45101a144
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-02-08 17:17:02 +0530

    Add user registration and login routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+// Register user
+router.post("/register", (req, res) => {
+    res.send("User registered");
+});
+
+// Login user
+router.post("/login", (req, res) => {
+    res.send("User logged in");
+});
+
+module.exports = router;
 
