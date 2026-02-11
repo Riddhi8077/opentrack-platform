e664c40e22ce3a3fd02445d93c47b454a384d9b6
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-02-11 21:18:34 +0530

    Add Roadmap model with schema definition

--- server/models/Roadmap.js
+++ server/models/Roadmap.js
@@ -1,1 +1,19 @@
+import mongoose from "mongoose";
+
+const roadmapSchema = new mongoose.Schema({
+  topic: {
+    type: String,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+export default mongoose.model("Roadmap", roadmapSchema);
 
