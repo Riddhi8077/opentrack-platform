2a2c6a3d551e7c8a667c2f74bb2bbf9d52413deb
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-02-11 21:17:25 +0530

    Add Program model with schema definition

--- server/models/Program.js
+++ server/models/Program.js
@@ -1,1 +1,26 @@
+import mongoose from "mongoose";
+
+const programSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  company: {
+    type: String
+  },
+  status: {
+    type: String,
+    enum: ["Applied", "In Progress", "Selected", "Rejected"],
+    default: "Applied"
+  },
+  deadline: {
+    type: Date
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+export default mongoose.model("Program", programSchema);
 
