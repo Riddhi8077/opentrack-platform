ffa882efc60a6cf3510950a09ad4854fd870a6fb
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-02-11 21:22:26 +0530

    Change import/export syntax in Roadmap.js

--- server/models/Roadmap.js
+++ server/models/Roadmap.js
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const roadmapSchema = new mongoose.Schema({
-  topic: {
-    type: String,
-    required: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
-  }
+    topic: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
 });
 
-export default mongoose.model("Roadmap", roadmapSchema);
-
+module.exports = mongoose.model("Roadmap", roadmapSchema);
